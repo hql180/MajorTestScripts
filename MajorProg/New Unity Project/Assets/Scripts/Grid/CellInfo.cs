1def6c13ef38342edec7a343b960759f5cae108a
Huy Le <huyql@hotmail.com>
2017-07-06 16:56:56 +0930

    started hexgrid manager

---
+++ MajorProg/New Unity Project/Assets/Scripts/Grid/CellInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellInfo : MonoBehaviour
+{
+	public Vector2 gridPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
